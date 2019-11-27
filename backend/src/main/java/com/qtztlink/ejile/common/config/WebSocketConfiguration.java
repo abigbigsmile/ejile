89eb372855f4d85ef16b7575337d01eb7216e32b
yhuihu <35516186+yhuihu@users.noreply.github.com>
2019-11-27 15:01:36 +0800

    WebSocketSession

---
+++ backend/src/main/java/com/qtztlink/ejile/common/config/WebSocketConfiguration.java
@@ -0,0 +1,30 @@
+package com.qtztlink.ejile.common.config;
+
+import com.qtztlink.ejile.common.handler.WebSocketHandshakeInterceptor;
+import com.qtztlink.ejile.common.websocket.MyWebSocket;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Tiger
+ * @date 2019-11-25
+ * @see com.qtztlink.ejile.common.config
+ **/
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+    @Resource
+    MyWebSocket myWebSocket;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        webSocketHandlerRegistry.addHandler(myWebSocket, "/chat").setAllowedOrigins("*")
+                .addInterceptors(new HttpSessionHandshakeInterceptor())
+                .addInterceptors(new WebSocketHandshakeInterceptor());
+    }
+}
