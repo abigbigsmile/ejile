1e493955812c73009cfe08e3c573f42273d0731b
yhuihu <35516186+yhuihu@users.noreply.github.com>
2019-11-27 15:08:42 +0800

    handler

---
+++ backend/src/main/java/com/qtztlink/ejile/common/handler/WebSocketHandshakeInterceptor.java
@@ -0,0 +1,32 @@
+package com.qtztlink.ejile.common.handler;
+
+import com.qtztlink.ejile.common.authentication.JWTUtils;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author Tiger
+ * @date 2019-11-25
+ * @see com.qtztlink.ejile.common.handler
+ **/
+public class WebSocketHandshakeInterceptor implements HandshakeInterceptor {
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object
+            > attributes) {
+        UriComponents uriComponents = UriComponentsBuilder.fromUri(request.getURI()).build();
+        String token = Objects.requireNonNull(uriComponents.getQueryParams().getFirst("token")).replace("%22", "");
+        return JWTUtils.getUserId(token) != null;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+
+    }
+}
