93c3036c7df2d5a20b3d434fb88cb7f9d9882b9f
yhuihu <35516186+yhuihu@users.noreply.github.com>
2019-11-27 15:40:27 +0800

    error

--- backend/src/main/java/com/qtztlink/ejile/system/service/communication/impl/MessageServiceImpl.java
+++ backend/src/main/java/com/qtztlink/ejile/system/service/communication/impl/MessageServiceImpl.java
@@ -3,6 +3,7 @@ package com.qtztlink.ejile.system.service.communication.impl;
 import com.qtztlink.ejile.system.dao.communication.MessageDao;
 import com.qtztlink.ejile.system.model.communication.MessageContact;
 import com.qtztlink.ejile.system.service.communication.MessageService;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
@@ -11,6 +12,7 @@ import javax.annotation.Resource;
  * @date 2019-11-26
  * @see com.qtztlink.ejile.system.service.communication.impl
  **/
+@Service
 public class MessageServiceImpl implements MessageService {
     @Resource
     MessageDao messageDao;
