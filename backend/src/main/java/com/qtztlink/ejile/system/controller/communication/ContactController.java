bcb54bebadfea640eecfc7cb17ca464ef46745de
yhuihu <35516186+yhuihu@users.noreply.github.com>
2019-11-29 09:20:31 +0800

    商家互动bug

--- backend/src/main/java/com/qtztlink/ejile/system/controller/communication/ContactController.java
+++ backend/src/main/java/com/qtztlink/ejile/system/controller/communication/ContactController.java
@@ -3,7 +3,11 @@ package com.qtztlink.ejile.system.controller.communication;
 import com.qtztlink.ejile.common.bean.ResponseBean;
 import com.qtztlink.ejile.common.exception.UnauthorizedException;
 import com.qtztlink.ejile.system.model.communication.Contact;
+import com.qtztlink.ejile.system.model.user.Consumer;
+import com.qtztlink.ejile.system.model.user.Shop;
 import com.qtztlink.ejile.system.service.communication.ContactService;
+import com.qtztlink.ejile.system.service.user.ConsumerService;
+import com.qtztlink.ejile.system.service.user.ShopService;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -20,34 +24,42 @@ import java.util.List;
 @Validated
 @RestController
 public class ContactController {
+
     @Autowired
     private ContactService contactService;
+    @Autowired
+    private ShopService shopService;
+    @Autowired
+    private ConsumerService consumerService;
 
     @RequestMapping("/contact/getInfoBySidCid")
     @ResponseBody
-    public ResponseBean getContactInfoBySidCid(
-            int sid,
-            int cid
-    ){
-        List<Contact> contactList = this.contactService.queryContactBySIDCID(sid,cid);
-        if(contactList!=null){
-            return new ResponseBean().code(200)
-                    .message("SUCCESS")
-                    .data(contactList);
-        } else {
-            return new ResponseBean().code(200)
-                    .message("Not find Goods")
-                    .data(new ArrayList<>());
+    public ResponseBean getContactInfoBySidCid(int sid, int cid) {
+        List<Contact> contactList = this.contactService.queryContactBySIDCID(sid, cid);
+        if (contactList == null) {
+            contactList=new ArrayList<>();
+            Shop shop = shopService.queryShopById(sid);
+            Consumer consumer = consumerService.queryConsumerById(cid);
+            Contact contact = new Contact();
+            contact.setSid(shop.getId());
+            contact.setContent("你正在与商家：" + shop.getUsername() + "交谈");
+            contact.setSname(shop.getUsername());
+            contact.setCid(consumer.getId());
+            contact.setTime(new Date());
+            contact.setCname(consumer.getUsername());
+            contact.setState("0");
+            contactList.add(contact);
         }
+        return new ResponseBean().code(200)
+                .message("SUCCESS")
+                .data(contactList);
     }
 
     @RequestMapping("/contact/getInfoBySid")
     @ResponseBody
-    public ResponseBean getContactInfoBySid(
-            @RequestParam("uid") int sid
-    ){
+    public ResponseBean getContactInfoBySid(@RequestParam("uid") int sid) {
         List<Contact> contactList = this.contactService.queryContactBySID(sid);
-        if(contactList!=null){
+        if (contactList != null) {
             return new ResponseBean().code(200)
                     .message("SUCCESS")
                     .data(contactList);
@@ -60,11 +72,9 @@ public class ContactController {
 
     @RequestMapping("/contact/getInfoByCid")
     @ResponseBody
-    public ResponseBean getContactInfoByCid(
-            @RequestParam("uid") int cid
-    ){
+    public ResponseBean getContactInfoByCid(@RequestParam("uid") int cid) {
         List<Contact> contactList = this.contactService.queryContactByCID(cid);
-        if(contactList!=null){
+        if (contactList != null) {
             return new ResponseBean().code(200)
                     .message("SUCCESS")
                     .data(contactList);
@@ -77,12 +87,11 @@ public class ContactController {
 
     @RequestMapping("/contact/add")
     @ResponseBody
-    public ResponseBean addContact(
-            int cid,
-            int sid,
-            Date ctime,
-            @Pattern(regexp = "^[01]$", message = "{invalid}") String state, // 0为用户发，1为商家发
-            @Length(min = 1, max = 1024, message = "{range}") String content
+    public ResponseBean addContact(int cid,
+                                   int sid,
+                                   Date ctime,
+                                   @Pattern(regexp = "^[01]$", message = "{invalid}") String state, // 0为用户发，1为商家发
+                                   @Length(min = 1, max = 1024, message = "{range}") String content
     ) throws Exception {
         Contact contact = new Contact();
         contact.setCid(cid);
@@ -91,12 +100,12 @@ public class ContactController {
         contact.setState(state);
         contact.setContent(content);
         int res = this.contactService.add(contact);
-        if(res == 1){
+        if (res == 1) {
             return new ResponseBean()
                     .code(200)
                     .message("Success")
                     .data(1);
-        }else{
+        } else {
             throw new UnauthorizedException("发送失败，请联系管理员");
         }
     }
